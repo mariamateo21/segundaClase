04044bd577847b5c61e946d4024d00da35d670cb
mariamateo21 <mariafava2003@yahoo.com.ar>
2022-09-19 14:42:34 -0300

    Cambie mensaje de Bienvenida

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 export default function Landing(){
     return (
         <div>
-            <h1>Bienvenidos!</h1>
+            <h1>Hola, gracias por el curso!</h1>
             <Link to="/home">
                 <button> Home</button>
             </Link>
